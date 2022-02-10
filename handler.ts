f2d2c5da44ed2343284d73f1bba7ea6e32b73986
JackShort <john.short.tj@gmail.com>
2022-02-10 17:04:20 -0500

    going to add checks

--- handler.ts
+++ handler.ts
@@ -1,27 +1,63 @@
 import { Handler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import { GetItemInput } from 'aws-sdk/clients/dynamodb';
 
-export const tokenId: Handler = (event: any) => {
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'ivx-status';
+
+async function getItem(params: GetItemInput) {
+    try {
+        const data = await docClient.get(params).promise();
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
+
+export const tokenId: Handler = async (event: any) => {
     const id = event.pathParameters.tokenId;
+    let response: {};
 
     if (id.length == 0) {
-        const response = {
+        response = {
             statusCode: 301,
             headers: {
                 Location: 'https://token.iv-x.xyz/',
             },
         };
+    } else {
+        const params = {
+            TableName: tableName,
+            Key: {
+                tokenId: id,
+            },
+        };
 
-        return new Promise((resolve) => {
-            resolve(response);
-        });
+        try {
+            const data = await getItem(params);
+
+            if (Object.keys(data).length == 0) {
+                response = {
+                    statusCode: 301,
+                    headers: {
+                        Location: 'https://token.iv-x.xyz/' + id,
+                    },
+                };
+            } else {
+                response = {
+                    statusCode: 200,
+                    body: JSON.stringify(data),
+                };
+            }
+        } catch (err) {
+            response = {
+                statusCode: 301,
+                headers: {
+                    Location: 'https://token.iv-x.xyz/' + id,
+                },
+            };
+        }
     }
-
-    const response = {
-        statusCode: 301,
-        headers: {
-            Location: 'https://token.iv-x.xyz/' + id,
-        },
-    };
 
     return new Promise((resolve) => {
         resolve(response);
